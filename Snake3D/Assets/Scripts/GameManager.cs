23ee111a703a5138af4930feb0c4ed5187512169
BeneDev <seriousbenedikt@web.de>
2018-04-17 06:08:58 -0200

    Old Version

--- Snake3D/Assets/Scripts/GameManager.cs
+++ Snake3D/Assets/Scripts/GameManager.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     public static GameManager Instance;
 
-    public int winningPlayer;
 
-=======
->>>>>>> 97abd64b0bc780d00e5be931aa9badb1ba220b4b
-=======
->>>>>>> parent of 8defcd8... Merge branch 'master' of https://github.com/BeneDev/DemoProject
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+		if(!Instance)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
 	}
 	
 	// Update is called once per frame
