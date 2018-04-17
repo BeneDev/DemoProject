97abd64b0bc780d00e5be931aa9badb1ba220b4b
Aaron Zimmermann <aaron.zimmermann98@googlemail.com>
2018-04-17 09:51:09 +0200

    Revert "Merge branch 'master' of https://github.com/BeneDev/DemoProject"
    This reverts commit 8defcd89a8c01880cab1ad3e1ea6a846a63909df, reversing
    changes made to 0fc8ba457b8c62cada8538e786bbf690edd37053.

--- Snake3D/Assets/Scripts/GameManager.cs
+++ Snake3D/Assets/Scripts/GameManager.cs
@@ -4,19 +4,9 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-    public static GameManager Instance;
-
-
 	// Use this for initialization
-	void Awake () {
-		if(!Instance)
-        {
-            Instance = this;
-        }
-        else
-        {
-            Destroy(gameObject);
-        }
+	void Start () {
+		
 	}
 	
 	// Update is called once per frame
